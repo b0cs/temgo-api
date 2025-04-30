b0d45748b04059851b2eee762558737c341de984
b0cs <bocar.coly@temgo.fr>
2025-04-30 13:49:30 +0200

    Fix: banned clients another way

--- models/member.model.js
+++ models/member.model.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 const { Schema } = mongoose;
 
 // Schéma pour gérer les horaires journaliers des membres
